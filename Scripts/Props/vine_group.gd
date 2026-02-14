40ed8fa84d4d192eea871fd00bb698acd3a28eee
Mark Feineigle <mfeineigle@gmail.com>
2026-02-14 18:07:04 -0500

    add toggle for component control / UID over path

--- Scripts/Props/vine_group.gd
+++ Scripts/Props/vine_group.gd
@@ -3,16 +3,18 @@ extends Node3D
 @onready var mesh_nodes := []
 @export var wind_direction: Vector3 = Vector3(1,0,0) # default global direction
 
+@export var component_control: bool = false
 func _ready() -> void:
-	Wind.wind_state_changed.connect(_on_wind_state_changed)
-	Wind.wind_strength_changed.connect(_on_wind_strength_changed)
+	if not component_control: # Wind singleton control
+		Wind.wind_state_changed.connect(_on_wind_state_changed)
+		Wind.wind_strength_changed.connect(_on_wind_strength_changed)
 	# find all meshes with "vine" in the name
 	for child in get_children():
 		if child is MeshInstance3D and "vine" in str(child.name).to_lower():
 			mesh_nodes.append(child)
 	for mesh in mesh_nodes:
 		# assign the shader material
-		var mat = preload("res://Assets/materials/wind_vine_shader_material.tres").duplicate(true) as ShaderMaterial
+		var mat = preload("uid://ctwjsuabouwwl").duplicate(true) as ShaderMaterial
 		mesh.material_override = mat
 		# transform global wind direction into the mesh's local space
 		var local_dir = mesh.transform.basis.inverse() * wind_direction
@@ -21,6 +23,8 @@ func _ready() -> void:
 		var random_phase = randf() * TAU  # TAU = 2 * PI
 		mat.set_shader_parameter("phase_offset", random_phase)
 
+# Called via signal from Wind singleton
+# or directly via windComponent
 func _on_wind_strength_changed(strength: float) -> void:
 	for mesh in mesh_nodes:
 		var mat := mesh.material_override as ShaderMaterial
